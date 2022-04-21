12c6ef1f5e4a34b116fd1512cf6c2e4738e0cf25
Charmi Amipara <charmiamipara@Charmis-MacBook-Air.local>
2022-04-21 20:00:28 +0530

    final commit

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+from pathlib import Path
+env_path = Path('./.env')
+load_dotenv(dotenv_path=env_path)
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
